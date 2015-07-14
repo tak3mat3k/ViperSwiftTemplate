7825376c91059ed34245e6ddbfbe6e8998909d5d
Takeshi <takeshi.kajino@imaginamos.co>
2015-07-14 10:54:14 -0500

    view in swift done

--- ViperModule_Swift/viper_class_view.py
+++ ViperModule_Swift/viper_class_view.py
@@ -14,36 +14,20 @@ class VIPERView_Interface(ViperClass_Interface):
 class VIPERView_h(ViperClass):
 	
 	def fName(self):
-		return "View.h"
+		return "View.swift"
 
 	def cName(self):
 		return "View"
 
 	def folderPath(self):
 		return self.folder.path_module_ui_view
-
-	def inherit(self):
-		return " : UIView"
 
 	def baseImport(self):
-		return "<UIKit/UIKit.h>"
-
-
-class VIPERView_m(ViperClass_Imp):
-	
-	def fName(self):
-		return "View.m"
-
-	def cName(self):
-		return "View"
-
-	def folderPath(self):
-		return self.folder.path_module_ui_view
+		return "UIKit"
 
 class VIPERView(ViperClass):
 
 	viperPresenter = None
-
 	view_interface = None
 	view_h = None
 	
@@ -56,19 +40,13 @@ class VIPERView(ViperClass):
 
  		self.view_h = VIPERView_h(self.module_name, self.folder)
 
- 		self.view_h.addProtocol(self.viperPresenter.presenter_interface.className())
-
-		self.view_h.addImports(self.view_interface.fileName())
-
+		self.view_h.addInterface("UIView")
 		self.view_h.addInterface(self.view_interface.className())
 
-		prt_view_presenter = Property.nonatomic_weak_interface(self.viperPresenter.presenter_interface.className()) + "presenter;"
+		prt_view_presenter = Property.var(self.viperPresenter.presenter_interface.className(), "presenter")
 		self.view_h.addProperty(prt_view_presenter)
 
 		self.view_h.create()
-
-		view_m = VIPERView_m(self.module_name, self.folder, self.view_h) 
-		view_m.create()
 
 
 
